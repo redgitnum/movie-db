a8244c8533255a96225d9fcb9fc1e4f425599d26
rednum <rednum89@gmail.com>
2020-07-13 18:11:40 +0200

    basic personi info

--- src/components/main/Details/PeopleDetails.js
+++ src/components/main/Details/PeopleDetails.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { fetchDetails, resetStore } from '../../../actions'
 import { connect } from 'react-redux';
+import placeholder from '../../../assets/placeholder.svg';
+
 
 const mapStateToProps = state => state;
 const mapDispatchToProps = {
@@ -18,11 +20,42 @@ class PeopleDetails extends React.Component{
         this.props.resetStore()
     }
 
+    imageLoaded = (e) => {
+        let placeholder = e.target.parentNode.childNodes[0];
+        let img = e.target;
+        placeholder.style.display = 'none';
+        img.style.display = 'block'
+    }
+
     render(){
         console.log(this.props.details)
         return(
-            <div>
-                {this.props.details.name}
+            <div className="details">
+                <div className='basic'>
+                    <div className="poster">
+                        <div className="placeholder"></div>
+                        <img onLoad={this.imageLoaded} alt="" src={this.props.details.entry.profile_path ? "https://image.tmdb.org/t/p/w300" + this.props.details.entry.profile_path : placeholder}></img>
+                    </div>
+                    <div className="info">
+                        <div className="name">
+                            {this.props.details.entry.name}
+                        </div>
+                        <div className="dates">
+                            <div className="birth">
+                                Birthday: {this.props.details.entry.birthday}
+                            </div>
+                                {this.props.details.entry.deathday ? 
+                                <div className="death"> Day of death: {this.props.details.entry.deathday}</div> 
+                                : null}
+                        </div>
+                        <div className="overview">
+                            <div className="title">
+                                Biography
+                            </div>
+                            {this.props.details.entry.biography}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
